2b7831352140337b73f5fa22e502df76c164c856
hzq <13402318457@163.com>
2019-07-25 14:33:22 +0800

    新增

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-let name = 'liuliu'
-const a = 'ewqewqewq'
-const fun = () => {
-    let e = 123
-    console.log(e)
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(500)
-        }, 5000)
+export default () => {
+    let name = 'liuliu'
+    const a = 'ewqewqewq'
+    const fun = () => {
+        let e = 123
+        console.log(e)
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(500)
+            }, 5000)
+        })
+    }
+    fun().then(data => {
+        console.log(data)
     })
+    console.log(name)
 }
-fun().then(data => {
-    console.log(data)
-})
-console.log(name)
